453b6d619c9cbf229933028d0b2c3bd39a635720
ziadelidrissi <ziadelidrissi1@gmail.com>
2024-04-24 20:23:35 +0200

    feat: configuration of connection to Mongo database

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 
-app.listen(process.env.PORT, () => {
-    console.log(`App listening on port ${process.env.BASE_URL}:${process.env.PORT}`);
-});
+// router
+app.get('/', (req, res) => {
+    res.send('Hello NODE-API')
+})
+
+app.get('/blog', (req, res) => {
+    res.send('Hello blog')
+})
+
+
+mongoose.
+connect('mongodb+srv://admin:sVwtl9EdeVTr0YvE@node-api.uadbae6.mongodb.net/Node-API?retryWrites=true&w=majority&appName=NODE-API')
+.then(() => {
+    console.log('connected to MongoDB');
+    app.listen(process.env.PORT, () => {
+        console.log(`App listening on port ${process.env.BASE_URL}:${process.env.PORT}`);
+    });
+}).catch((error) => {
+    console.log(error);
+})
